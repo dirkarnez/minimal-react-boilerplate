47df2c7b19d9fa438939fd1b9e6d35fd6cf72b90
dirkarnez <smalldirkalex@gmail.com>
2019-03-05 15:37:57 +0800

    - init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>12445343</p>
+      </div>
+    );
+  }
+}
